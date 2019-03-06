24b50b3dd89fad796592a16afd08ad039239a8fc
MitchTODO <42443446+MitchTODO@users.noreply.github.com>
2019-03-06 14:19:40 -0700

    Add files via upload

---
+++ Meme1.1/memeConstruct.swift
@@ -0,0 +1,17 @@
+//
+//  saveMeme.swift
+//  Meme1.1
+//
+//  Created by mitch on 2/13/19.
+//  Copyright © 2019 mitch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct memeConstruct {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
